f48b4f73162e32272f6beb33ded2dde2dbca933b
Bogdan <rotundbogdan26@gmail.com>
2022-10-11 12:57:45 +0300

    Added New Project

---
+++ Lab_11_10/Lab_11_10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 256;
+            int pad = 8;
+            int no = 42;
+            ShowPad(no, pad);
+        }
+        private static void ShowPad(int no, int pad)
+        {
+            Stack<int> stack = new Stack<int>();
+            while (no != 0)
+            {
+                stack.Push(no%2);
+                no /= 2;
+            }
+            
+        }
+    }
+}
